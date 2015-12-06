f6ad756e0c79d5ff60d51b5bbc51334a96f61439
Michael Gilliland <mjg.py3@gmail.com>
2015-12-06 01:34:35 +0000

    For now, simply say that the first header is the title (rarely true)

--- src/Classify.hs
+++ src/Classify.hs
@@ -12,12 +12,15 @@ escapeQuotes [] = []
 escapeQuotes ('"':rest) = '\\':'"':escapeQuotes rest
 escapeQuotes (x:rest) = x:escapeQuotes rest
 
+removeNewlines :: String -> String
+removeNewlines = filter (`notElem` "\n\r")
+
 isHeader :: Tag String -> Bool
 isHeader (TagOpen (firstChar:secondChar:_) _) = firstChar `elem` "hH" && secondChar `elem` "123456"
 isHeader _ = False
 
 takeTillClose :: Tag String -> [Tag String] -> [Tag String]
-takeTillClose (TagOpen name _) (_:tags) = go 1 tags
+takeTillClose (TagOpen name _) tags = go 1 tags
   where
   go 1 (endTag@(TagClose endName):rest) = if endName == name then [endTag] else endTag:go 1 rest
   go n (endTag@(TagClose endName):rest) = endTag:go (if endName == name then (n-1) else n) rest
@@ -34,11 +37,13 @@ extractHeaderTags (tag:tags) = (if isHeader tag then takeTillClose tag tags:rest
 formatAsJson :: Classifications -> String
 formatAsJson c = concat [
   "{\n"
-  ,"  \"title\": ", "\"", escapeQuotes $ title c ,"\"\n"
+  ,"  \"title\": ", "\"", escapeQuotes $ removeNewlines $ title c ,"\"\n"
   ,"}"
   ]
 
-classify formatter rawHtml = formatter $ Classifications { title = "Foo" }
+classify formatter rawHtml = formatter $ Classifications { title = (innerText $ head headers) }
+  where
+  headers = extractHeaderTags $ parseTags rawHtml
 
 main = do
   args <- getArgs
