4b9111025619d7f6f38f399b806f98f04b5512cd
Michael Gilliland <mjg.py3@gmail.com>
2015-12-06 00:14:06 +0000

    Extract tags till end tag

--- src/Classify.hs
+++ src/Classify.hs
@@ -16,8 +16,20 @@ isHeader :: Tag String -> Bool
 isHeader (TagOpen (firstChar:secondChar:_) _) = firstChar `elem` "hH" && secondChar `elem` "123456"
 isHeader _ = False
 
-extractHeaderTags :: [Tag String] -> [Tag String]
-extractHeaderTags = filter isHeader
+takeTillClose :: Tag String -> [Tag String] -> [Tag String]
+takeTillClose (TagOpen name _) (_:tags) = go 1 tags
+  where
+  go 1 (endTag@(TagClose endName):rest) = if endName == name then [endTag] else endTag:go 1 rest
+  go n (endTag@(TagClose endName):rest) = endTag:go (if endName == name then (n-1) else n) rest
+  go n (openTag@(TagOpen openName _):rest) = openTag:go (if openName == name then (n+1) else n) rest
+  go n (tag:tags) = tag:go n tags
+  go n [] = []
+takeTillClose _ _ = []
+
+extractHeaderTags :: [Tag String] -> [[Tag String]]
+extractHeaderTags [] = []
+extractHeaderTags (tag:tags) = (if isHeader tag then takeTillClose tag tags:rest else rest)
+  where rest = extractHeaderTags tags
 
 formatAsJson :: Classifications -> String
 formatAsJson c = concat [
