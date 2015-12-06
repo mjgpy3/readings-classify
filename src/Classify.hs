f49353ab8a56f59413d9fbadf00226681f5dbc8f
Michael Gilliland <mjg.py3@gmail.com>
2015-12-06 02:37:48 +0000

    Start on factors and fix some warnings

--- src/Classify.hs
+++ src/Classify.hs
@@ -1,11 +1,29 @@
 module Main where
 
+import qualified Data.Map.Strict as M
+
 import System.Environment
 import Text.HTML.TagSoup
 
 data Classifications = Classifications {
     title :: String
   }
+
+data TitleFactors = TitleFactors {
+    headerText :: String
+    ,occursOnce :: Bool
+  }
+  deriving Show
+
+type HeaderContext = M.Map String Int
+
+headerToTitleFactors :: [Tag String] -> HeaderContext -> TitleFactors
+headerToTitleFactors headerTag counts = TitleFactors {
+    headerText = innerText headerTag
+    ,occursOnce = M.findWithDefault 0 tagName counts == 1
+  }
+    where
+    (TagOpen tagName _:_) = headerTag
 
 escapeQuotes :: String -> String
 escapeQuotes [] = []
@@ -20,19 +38,28 @@ isHeader (TagOpen (firstChar:secondChar:_) _) = firstChar `elem` "hH" && secondC
 isHeader _ = False
 
 takeTillClose :: Tag String -> [Tag String] -> [Tag String]
-takeTillClose (TagOpen name _) tags = go 1 tags
+takeTillClose tag@(TagOpen name _) tagsFollowing = tag:go 1 tagsFollowing
   where
   go 1 (endTag@(TagClose endName):rest) = if endName == name then [endTag] else endTag:go 1 rest
   go n (endTag@(TagClose endName):rest) = endTag:go (if endName == name then (n-1) else n) rest
   go n (openTag@(TagOpen openName _):rest) = openTag:go (if openName == name then (n+1) else n) rest
-  go n (tag:tags) = tag:go n tags
-  go n [] = []
+  go n (current:tags) = current:go n tags
+  go _ [] = []
 takeTillClose _ _ = []
 
 extractHeaderTags :: [Tag String] -> [[Tag String]]
 extractHeaderTags [] = []
 extractHeaderTags (tag:tags) = (if isHeader tag then takeTillClose tag tags:rest else rest)
   where rest = extractHeaderTags tags
+
+countHeaders :: [[Tag String]] -> HeaderContext
+countHeaders ((TagOpen name _:_):headers) = 
+  let 
+    counts = countHeaders headers 
+    n = maybe 1 (+ 1) (M.lookup name counts)
+  in
+    M.insert name n counts
+countHeaders _ = M.empty
 
 formatAsJson :: Classifications -> String
 formatAsJson c = concat [
@@ -41,10 +68,19 @@ formatAsJson c = concat [
   ,"}"
   ]
 
-classify formatter rawHtml = formatter $ Classifications { title = (innerText $ head headers) }
+findTitle :: [[Tag String]] -> String
+findTitle headers = innerText $ head headers
   where
-  headers = extractHeaderTags $ parseTags rawHtml
+  counts = countHeaders headers
+  factors = map (`headerToTitleFactors` counts) headers
 
+classify :: (Classifications -> String) -> String -> String
+classify formatter rawHtml = formatter $ Classifications { title = findTitle headers }
+  where
+  tags = parseTags rawHtml
+  headers = extractHeaderTags tags
+
+main :: IO ()
 main = do
   args <- getArgs
   case args of
